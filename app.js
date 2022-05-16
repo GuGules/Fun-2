7c9b0de2d3c3bb66a66e66eecd22472fecc84244
GuGules <89942177+GuGules@users.noreply.github.com>
2022-05-16 14:59:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function survolee(x){
         src_2=document.getElementById("i3").src;
         document.getElementById("i3").src=src_1;
         document.getElementById("i2").src=src_2;
+    } else if (x==3){
+        document.getElementById("i1").src=document.getElementById("i4").src;
+        document.getElementById("i2").src=document.getElementById("i4").src;
+        document.getElementById("i3").src=document.getElementById("i4").src;
     } else {
         src_1=document.getElementById("i1").src;
         src_2=document.getElementById("i4").src;
@@ -16,9 +20,19 @@ function survolee(x){
     }
 }
 
-function reset(x){
-    document.getElementById("i1").src="i1.jpeg";
-    document.getElementById("i2").src="i2.jpeg";
-    document.getElementById("i3").src="i3.jpeg";
-    document.getElementById("i4").src="i4.jpeg";
+function random_int(xa,xb){
+    var min=xa; 
+    var max=xb;  
+    var random = Math.floor(Math.random()*100)+min; 
+    return random.toString()
 }
+
+function generator(){
+    document.getElementById("i1").src=random_int(1,10)+".jpeg";
+    document.getElementById("i2").src=random_int(1,10)+".jpeg";
+    document.getElementById("i3").src=random_int(1,10)+".jpeg";
+    document.getElementById("i4").src=random_int(1,10)+".jpeg";
+    return True
+}
+
+generator()
